36a89604e07477c1dc7aadb98d862b698293ddf5
Jezza34000 <57314417+Jezza34000@users.noreply.github.com>
2021-08-26 14:36:43 +0200

    Update filenotif.class.php

--- core/class/filenotif.class.php
+++ core/class/filenotif.class.php
@@ -56,15 +56,18 @@ class filenotif extends eqLogic {
           $this->checkAndUpdateCmd('info_filecount', $deltaCount);
           log::add('filenotif', 'debug', 'Comptage | Delta='.$deltaCount.' OLD='.$oldCount." NEW=".$newCount);
           $notifmode = $this->getConfiguration('notifmode');
-          log::add('filenotif', 'debug', 'Notif mode = '.$notifmode);
+
           if (($notifmode == "newfile") && ($deltaCount > 0)) {
             // New file ADDED
+            log::add('filenotif', 'debug', 'ADD case mode='.$notifmode);
             filenotif::raisenotif(10);
           } elseif (($notifmode == "delfile") && ($deltaCount < 0)) {
             // File Deleted
+            log::add('filenotif', 'debug', 'DEL case mode='.$notifmode);
             filenotif::raisenotif(10);
           } else {
             // BOTH DEL&NEW
+            log::add('filenotif', 'debug', 'ELSE case mode='.$notifmode);
             filenotif::raisenotif(10);
           }
       } else {
